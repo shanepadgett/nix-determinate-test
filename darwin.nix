7e2a87f9bd317d4a52acdd76f4382a45f6d99a70
Shane Padgett <contact@shanepadgett.dev>
2025-08-11 19:45:37 -0400

    feat: update apple config

--- darwin.nix
+++ darwin.nix
@@ -111,7 +111,7 @@
       minimize-to-application = true;
       persistent-apps = [
         "/Applications/Brave Browser.app"
-        "/Applications/Home Manager Trampolines/Visual Studio Code.app"
+        "/Applications/Home Manager Trampolines/Visual Studio Code"
         "/Applications/Zed.app"
         "/Applications/Warp.app"
         "/Applications/Bruno.app"
